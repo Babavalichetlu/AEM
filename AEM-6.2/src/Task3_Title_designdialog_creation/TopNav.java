f522371a4cb1f4609bfadacde5dd291705f9d484
10642502 <10642502@INFCHSZA0517.lntinfotech.com>
2020-02-05 20:22:01 +0530

    Title and Designdialog creationcode and images

---
+++ AEM-6.2/src/Task3_Title_designdialog_creation/TopNav.java
@@ -0,0 +1,40 @@
+package apps.ComplexnavDesign.components.content.site-topnav;
+import java.util.*;
+import java.util.Iterator;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class TopNav extends WCMUsePojo{
+    public static final Logger LOGGER = LoggerFactory.getLogger(TopNav.class);
+    LOGGER.info("********Adding the repository******");
+    private List<Page> items = new ArrayList<Page>();
+    private Page rootPage;
+
+    // Initializes the navigation
+    @Override
+    public void activate() throws Exception {
+        rootPage = getCurrentPage().getAbsoluteParent(2);
+
+        if (rootPage == null) {
+        	rootPage = getCurrentPage();
+        }
+        
+        Iterator<Page> childPages = rootPage.listChildren(new PageFilter(getRequest()));
+	   	while (childPages.hasNext()) {
+			items.add(childPages.next());
+	   	}
+    }
+
+    // Returns the navigation items
+    public List<Page> getItems() {
+        return items;
+    }
+    // Returns the navigation root
+    public Page getRoot() {
+        return rootPage;
+    }
+}
